933d235e5f009621d767424893525916b5b8e3af
Gabriel <gabriel.almeida673@outlook.com>
2022-08-18 21:47:03 -0300

    projeto de desafio poo

---
+++ Desafio - POO/desafio-dio-POO/src/br/com/dio/dominio/Mentoria.java
@@ -0,0 +1,31 @@
+package br.com.dio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria extends Conteudo {
+	private LocalDate date;
+
+	@Override
+	public double calcularXP() {
+		return XP_PADRAO + 20d;
+	}
+
+	public Mentoria() {
+
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+
+		return "Mentoria [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", data=" + date + "]";
+	}
+
+}
